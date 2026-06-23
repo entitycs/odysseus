2b2210ed8cd455c9dffc39a7119177e7467d3921
entitycs <entitycs@gmail.com>
2026-06-23 15:11:52 -0600

    Update app.py to be svelte-aware/svelte-first (temp - *any* route)
    fix(css): set overflow-y:auto in main - restores footer
    
    fix(css): Aside from overflow-y, let main retain original styling
    
    fix(css): force main display type to flex - restores original layout -?

--- web/routes/+layout.svelte
+++ web/routes/+layout.svelte
@@ -2135,14 +2135,8 @@ let { children } = $props();
 	}
 
 	main {
-		flex: 1;
+		overflow-y: auto;
 		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
 	}
 
 	footer {
